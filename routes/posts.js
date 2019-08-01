3c9d4f5c2cb1ae4c8eefce100cb0948059c88540
Thomas <thomasdufayet3@gmail.com>
2019-08-01 15:01:31 +0200

    base API + authentication

---
+++ routes/posts.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const verify = require('./verifyToken')
+
+router.get('/', verify, (req,res) => {
+    res.json({posts: {title: 'test', description : 'blablabablabla'}})
+})
+module.exports = router;
